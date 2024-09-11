4074e19dcbef7d5c85d02ed755c2a4bcb71faf75
RyanX136 <xxu253@gmail.com>
2024-09-11 14:38:45 -0500

    Update OddEvenHelperTest.java

--- test/testingbasics/OddEvenHelperTest.java
+++ test/testingbasics/OddEvenHelperTest.java
@@ -9,16 +9,21 @@ public class OddEvenHelperTest {
     void evenNumbers() {
         assertEquals(true, OddEvenHelper.isEven(24));
         assertEquals(false, OddEvenHelper.isOdd(24));
+        assertEquals(true, OddEvenHelper.isEven(-24));
+        assertEquals(false, OddEvenHelper.isOdd(-24));
     }
 
     @Test
     void oddNumbers() {
         assertEquals(false, OddEvenHelper.isEven(17));
         assertEquals(true, OddEvenHelper.isOdd(17));
+        assertEquals(false, OddEvenHelper.isEven(-17));
+        assertEquals(true, OddEvenHelper.isOdd(-17));
     }
 
     @Test
     void zero() {
         assertEquals(true, OddEvenHelper.isEven(0));
+        assertEquals(false, OddEvenHelper.isOdd(0));
     }
 }
